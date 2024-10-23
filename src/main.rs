8071c5ea4f0ad9302c07a8398406e3267c3dae57
faliah <leogoldf21@gmail.com>
2024-10-23 22:49:16 +0100

    updated

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::parser;
 use serde::{Deserialize, Serialize};
 use std::fs::File;
+use serde_json;
 #[derive(Serialize, Deserialize, Debug)]
 struct Task {
     description: String,
@@ -16,7 +17,7 @@ fn main() {
 
 fn save_tasks(tasks: &Vec<Task>) -> std::io::Result<()> {
     let file = File::create("tasks.json")?;
-    // let json = serde_json::to_string(tasks)?;
-    // write!(file, "{}", json)?;
+    let json = serde_json::to_string(tasks)?;
+    write!(file, "{}", json)?;
     Ok(())
 }
